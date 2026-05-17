e688d3b21a34644495d31b3b34a240402639f6a1
ShantanuBhapkar <shantanubhapkar@gmail.com>
2026-05-17 23:49:21 +0530

    postgres pool update

--- src/config/postgres.ts
+++ src/config/postgres.ts
@@ -3,6 +3,8 @@ const { Pool } = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL ,
+ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // in production we want to use SSL but in development we can skip it for simplicity. This also allows us to connect to local Postgres without SSL.
+});
 
 export default pool;
